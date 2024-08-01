37d7bac99dd92b46447ceab2cef619d7f2ed2daa
Vinu Cyril <vinucyril@gmail.com>
2024-08-01 20:43:59 +0530

    reversed order

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,45 @@ function App() {
     <>
       <div className="py-[30px] px-[20px]  flex justify-between">
         <div>
-          <h1 className=" text-[40px] text-blue-600 font-bold">
-            Pentagon Studio
-          </h1>
-          <div>
-            <address>
-              4/108<br></br> Kadakkarapally PO<br></br> Cherthala,<br></br>{" "}
-              Alappuzha, 688529<br></br>
-              GSTIN: 32CWCPA6689R1ZX
-            </address>
+          <address>
+            <span className=" text-[20px] font-medium">Vinu Cyril</span>
+            <br></br>
+            No:60, 4th cross<br></br>
+            Brindavan Layout<br></br>
+            Horamavu Kalkere main road<br></br>
+            Bangalore 560043
+          </address>
+          <div className=" mt-8">
+            <h3>
+              <span className="text-[20px]">PAN: </span> BVOPC1207M
+            </h3>
+            <h3>
+              <span className="text-[20px]">Bank Acc: </span> 50100255641330
+            </h3>
+            <h3>
+              <span className="text-[20px]">IFSC code: </span>
+              HDFC0004075
+            </h3>
+            <h3>
+              <span className="text-[20px]">UPI ID: </span>
+              7560824678@ybl
+            </h3>
           </div>
           <div className=" mt-8">
             <h2 className=" text-blue-600">To:</h2>
-            <address>
-              Vinu Cyril<br></br>
-              No:60, 4th cross<br></br>
-              Brindavan Layout<br></br>
-              Horamavu Kalkere main road<br></br>
-              Bangalore 560043
-            </address>
-            <div className=" mt-8">
-              <h3>
-                <span className="text-[20px]">PAN: </span> BVOPC1207M
-              </h3>
-              <h3>
-                <span className="text-[20px]">Bank Acc: </span> 50100255641330
-              </h3>
-              <h3>
-                <span className="text-[20px]">IFSC code: </span>
-                HDFC0004075
-              </h3>
-              <h3>
-                <span className="text-[20px]">UPI ID: </span>
-                7560824678@ybl
-              </h3>
+            <h1 className=" text-[40px] text-blue-600 font-bold">
+              Pentagon Studio
+            </h1>
+            <div>
+              <address>
+                4/108<br></br> Kadakkarapally PO<br></br> Cherthala,<br></br>{" "}
+                Alappuzha, 688529<br></br>
+                GSTIN: 32CWCPA6689R1ZX
+              </address>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-5">
+        <div className="flex flex-col gap-2">
           <h1 className=" text-[30px] text-blue-600 font-bold underline">
             Invoice
           </h1>
@@ -48,7 +49,7 @@ function App() {
             <span className=" text-blue-600  font-bold">Date:</span> 1 Aug 2024
           </p>
           <div className=" flex gap-2">
-            <h2 className=" text-blue-600">For:</h2>
+            <h2 className=" text-blue-600 font-semibold">For:</h2>
             <p>Web Development</p>
           </div>
         </div>
